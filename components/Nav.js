64b9fdfbc55dd21e5d14e02913951eb32928e0b8
andothergames <morrisjoy@hotmail.co.uk>
2024-07-01 10:04:42 +0100

    add Header and Nav components

---
+++ components/Nav.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import { styles } from "../styles"; 
+
+export default function Nav() {
+
+    return (
+        <View style={styles.container}>
+            <Text>Nav bar</Text>
+            
+        </View>
+        
+    )
+}
