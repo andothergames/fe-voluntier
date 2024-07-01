399e61c2752ee265f737b4becf4fa8ce1d7fe35a
andothergames <morrisjoy@hotmail.co.uk>
2024-07-01 14:54:53 +0100

    feat: (wip) nav bar and add png icons

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
       <TopNav />
       <Header />
       <Listings />
-      {/* <Nav /> */}
+      <Nav />
     </View>
   );
 }
---
+++ components/MyListings.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import { styles } from "../styles";
+
+export default function MyListings() {
+  return (
+    <View style={styles.container}>
+      <Text>My Listings</Text>
+    </View>
+  );
+}
