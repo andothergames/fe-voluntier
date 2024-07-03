cde3e425647785adfc05213541ac3a47d4124b53
Tom Follows <tomjfollows@gmail.com>
2024-07-03 14:53:44 +0100

    feat: add contact us form

---
+++ styles/contactStyles.js
@@ -0,0 +1,91 @@
+import { StyleSheet } from "react-native";
+
+export const contactStyles = StyleSheet.create({
+  container: {
+    backgroundColor: "#ffff",
+  },
+  textForm: {
+    margin: 10,
+    padding: 10,
+    backgroundColor: "white",
+    borderRadius: 10,
+  },
+  form: {
+    margin: 10,
+    padding: 20,
+    backgroundColor: "#f5f5f5",
+    borderRadius: 10,
+  },
+  input: {
+    marginBottom: 20,
+  },
+  textInput: {
+    backgroundColor: "#fff",
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    borderRadius: 12,
+    fontSize: 15,
+    fontWeight: "500",
+    color: "#222",
+  },
+  messageTextInput: {
+    backgroundColor: "#fff",
+    paddingBottom: 80,
+    paddingHorizontal: 16,
+    borderRadius: 12,
+    fontSize: 15,
+    fontWeight: "500",
+    color: "#222",
+  },
+  inputLabel: {
+    fontSize: 17,
+    color: "#222",
+    marginBottom: 8,
+    marginTop: 10,
+  },
+  title: {
+    marginTop: 20,
+    marginBottom: 40,
+    textAlign: "center",
+    fontSize: 20,
+    color: "#001FFF",
+  },
+  button: {
+    marginTop: 20,
+    marginBottom: 40,
+    marginRight: "auto",
+    marginLeft: "auto",
+    backgroundColor: "#7BB9F8",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 25,
+    width: "40%",
+    alignContent: "center",
+  },
+  white: {
+    color: "#fff",
+    textAlign: "center",
+  },
+  inputControl: {
+    backgroundColor: "#fff",
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    borderRadius: 12,
+    fontSize: 15,
+    fontWeight: "500",
+    color: "#222",
+  },
+  switch: {
+    marginLeft: "auto",
+    marginRight: 10,
+  },
+  switchText: {
+    color: "#383838",
+    textAlign: "right",
+    paddingRight: 10,
+  },
+  error: {
+    textAlign: "center",
+    color: "#001FFF",
+  },
+});
