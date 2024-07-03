ed93d7862843957b91a6d41800a138e8676643ec
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-07-03 16:36:14 +0100

    took off the border from the header

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,13 @@
-import "react-native-gesture-handler";
-import { View, Image, StyleSheet, Pressable } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import Login from "./Login";
+import 'react-native-gesture-handler';
+import { View, Image, StyleSheet, Pressable } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Login from './Login';
 
 export default function Header() {
   const navigation = useNavigation();
 
   const navigateToLogin = () => {
-    navigation.navigate("Login");
+    navigation.navigate('Login');
   };
 
   return (
@@ -21,17 +21,15 @@ export default function Header() {
 
       <View style={styles.logoContainer}>
         <Image
-          source={require("../assets/voluntierlogo.png")}
-          style={styles.logo}
-        ></Image>
+          source={require('../assets/voluntierlogo.png')}
+          style={styles.logo}></Image>
       </View>
 
       <View style={styles}>
         <Pressable onPress={navigateToLogin}>
           <Image
-            source={require("../assets/account-icon.png")}
-            style={styles.accountIcon}
-          ></Image>
+            source={require('../assets/account-icon.png')}
+            style={styles.accountIcon}></Image>
         </Pressable>
       </View>
     </View>
@@ -40,26 +38,25 @@ export default function Header() {
 
 const styles = StyleSheet.create({
   headerContainer: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     flex: 1 / 6,
     marginHorizontal: 5,
-    borderWidth: 1,
-    borderColor: "red",
+
     marginTop: 40,
   },
   logoContainer: {
     flex: 1,
-    height: "70%",
+    height: '70%',
   },
   settingsIcon: {
     width: 30,
     height: 30,
   },
   logo: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "contain",
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
   },
   accountIcon: {
     width: 30,
