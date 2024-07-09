64d485d8f7681547aff01ad5d5fc2ec96d23351f
Joy <32204909+andothergames@users.noreply.github.com>
2024-07-09 09:58:44 +0100

    feat: add org listings to badges page

--- screens/OrgBadgesScreen.js
+++ screens/OrgBadgesScreen.js
@@ -1,10 +1,42 @@
-import { View, Text } from "react-native";
+import { UserContext } from "../contexts/user-context";
+import { useContext, useState, useEffect } from "react";
+import { View, Pressable } from "react-native";
+import { getOrgListings } from "../api";
+import ListingCard from "../components/ListingCard";
+import { useNavigation } from "@react-navigation/native";
+import { listingStyles as styles } from "../styles/listingStyles";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function OrgBadgesScreen() {
+
+  const { user } = useContext(UserContext);
+  const [orgListings, setOrgListings] = useState([]);
+  const [err, setErr] = useState("");
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    getOrgListings(user.org_id, user.token)
+      .then((listings) => {
+        setOrgListings(listings);
+      })
+      .catch((error) => {
+        setErr(error);
+      });
+  }, []);
+
+  const handlePress = (listing) => {
+    navigation.navigate("SingleListing", { listing });
+  };
   
   return (
-    <View>
-      <Text>This will show the orgs listings along with the volunteers who have applied. The Org can either remove them or confirm their attendance to award hours/badges to vol.</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      {orgListings.map((listing) => {
+        return (
+          <View key={listing.list_id}>
+            <ListingCard listing={listing}></ListingCard>
+          </View>
+        )
+      })}
+    </ScrollView>
   );
 }
